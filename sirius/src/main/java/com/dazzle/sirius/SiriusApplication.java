852450a66d5d50dff4c42c966dca07b3372f6e17
Li_Jiahui <Li_Jiahui@outlook.com>
2018-11-19 19:37:48 +0800

    commit

---
+++ sirius/src/main/java/com/dazzle/sirius/SiriusApplication.java
@@ -0,0 +1,12 @@
+package com.dazzle.sirius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiriusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiriusApplication.class, args);
+	}
+}
