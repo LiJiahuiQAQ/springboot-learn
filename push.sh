d12f612d33bf66238d30c9736c8df45ac527c0be
Li_Jiahui <Li_Jiahui@outlook.com>
2018-11-19 14:35:39 +0800

    commit

--- push.sh
+++ push.sh
@@ -3,9 +3,7 @@ dirname $0
 cd `dirname $0`
 git init
 git add .
-git status 
 git commit -m "commit"
-git log
 git remote rm origin
 git remote add origin https://github.com/LiJiahuiQAQ/springboot-learn.git
 git commit -m "commit"
